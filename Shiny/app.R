1b18fc29a87e778252680ace5579140aa9a726cb
Y. Brian Lee <yxblee@users.noreply.github.com>
2020-02-23 18:18:17 -0800

    Update datasets and app; restructure directories; minor refactoring

--- Shiny/app.R
+++ Shiny/app.R
@@ -12,20 +12,24 @@ library(viridis)
 # Define UI for app that draws a histogram ----
 ui <- shinyUI(
   navbarPage(
-    # App title ----
-    title = "nCov 2019 Clustering (Singapore)",
+    # App title ---
+    title = "COVID-19 Clustering (Singapore)",
+    
     tabPanel("Network Diagram", 
              plotOutput(outputId = "networkPlot", width = "100%", height = "800"),
              selectInput(inputId = "subset", 
                          label = "Choose an attribute to colour the group by:",
-                         choices = c("age", "cluster",
-                                     "hospital"),
+                         choices = c("age", "cluster", "hospital"),
                          selected = "age"),
     ),
+    
     tabPanel("Heatmap",
              plotlyOutput(outputId = "heatmap", width = "100%", height = "1000"),
     ),
-    footer = HTML("<a href=\"https://github.com/yxblee/EpiCoronaHack_Cluster/blob/master/Data/nCov_Singapore_2019.csv\" style=\"font-size:24px\">Singapore COVID-19 Dataset (Updated as of February 19, 2020)</a>")
+    
+    # Footer link ---
+    footer = HTML("<a href=\"https://github.com/yxblee/EpiCoronaHack_Cluster/blob/master/Data/COVID-19_Singapore.csv\" style=\"font-size:24px\">
+                  Singapore COVID-19 Dataset (Updated as of February 19, 2020)</a>")
     
   )
     
@@ -43,10 +47,10 @@ ui <- shinyUI(
     #             selected = "age")
     #     
     #   
-    #   # Main panel for displaying outputs ----
+    #   # Main panel for displaying outputs ---
     #   mainPanel(
     #     
-    #     # Output: Plot ----
+    #     # Output: Plot ---
     #     plotOutput(outputId = "plot",  width = "100%")
     #     
     #   )
@@ -67,24 +71,24 @@ server <- function(input, output) {
   
   output$networkPlot <- renderPlot({
     
-    varNet = read.csv("nCovNet.csv")
+    varNet = read.csv("COVID-19_Singapore_Network_plot.csv")
     
     # bins <- seq(min(x), max(x), length.out = input$bins + 1)
     # varNet$age = as.character(varNet$age)
     # varNet$age = as.integer(varNet$age)
     # varNet$age_bin = bin(varNet$age,nbins=5)
     
-    varNet = read.csv("nCovNet.csv")
-    
     subset <- if(input$subset=="age") "age_bin" else input$subset
     
-    cov_net=ggplot(varNet,aes(x = x, y = y, xend = xend, yend = yend))+
-      geom_edges(aes(),subset(varNet,Related.cases!=""),linetype="solid")+
-      geom_nodes(aes(color=varNet[[subset]]),size=14)+
-      geom_text(aes(label=name),check_overlap = TRUE)+
-      guides(colour = guide_legend(title=subset, title.position="top",title.hjust = 0.5,override.aes = list(size=6)))+
-      scale_color_viridis_d(na.value="grey50")+
-      theme_bw()+
+    cov_net = ggplot(varNet, aes(x = x, y = y, xend = xend, yend = yend)) +
+      geom_edges(aes(), subset(varNet, Related.cases != ""), linetype="solid") +
+      geom_nodes(aes(color=varNet[[subset]]), size=14) +
+      geom_text(aes(label=name), check_overlap = TRUE) +
+      ggtitle("COVID-19 Singapore Cluster Network") +
+      guides(colour = guide_legend(title=subset, title.position="top", title.hjust = 0.5, override.aes = list(size=6))) +
+      scale_color_viridis_d(na.value="grey50") +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5)) + #centre main title
       theme(panel.grid = element_blank(),
             axis.title = element_blank(),
             axis.line = element_blank(),
@@ -93,8 +97,7 @@ server <- function(input, output) {
             panel.border = element_blank(),
             legend.position = "bottom",
             legend.direction = "horizontal",
-            legend.text=element_text(size=12)
-      )
+            legend.text=element_text(size=12))
     
     cov_net
 
@@ -103,28 +106,30 @@ server <- function(input, output) {
   
   output$heatmap <- renderPlotly({
     
-    varHeatmap = read.csv("heatmap_plot.csv")
+    varHeatmap = read.csv("COVID-19_Singapore_Heatmap_plot.csv")
     
-    varHeatmap$date <- factor(varHeatmap$date,levels=unique(varHeatmap$date))
-    varHeatmap$case <- factor(varHeatmap$case,levels=unique(varHeatmap$case))
+    varHeatmap$date <- factor(varHeatmap$date, levels=unique(varHeatmap$date))
+    varHeatmap$case <- factor(varHeatmap$case, levels=unique(varHeatmap$case))
     
     heatmap <- ggplot(
       varHeatmap, 
+      # aes(x = date, y = case, fill = status_word,
       aes(x = date, y = case, fill = status,
           text = paste("Case: ", case,
                        "<br>Date: ", date,
-                       "<br>Status: ", status,
+                       "<br>Status: ", status_word,
                        "<br>Cluster: ", cluster,
                        "<br>Citizenship: ", citizenship))) +
       geom_tile() +
       xlab(label = "Date") +
       ylab(label = "Cases") +
-      ggtitle("COVID-19 progression among Singapore cases") +
+      ggtitle("COVID-19 Progression Amongst Singapore Cases") +
       labs(fill = "Status") + #tile fill legend label
       theme(plot.title = element_text(hjust = 0.5)) + #centre main title
-      theme(axis.text.x = element_text(angle =60, hjust = 0.6, size = 8),
+      theme(axis.text.x = element_text(angle = 60, hjust = 0.6, size = 8),
             axis.ticks.x = element_blank(), #remove x axis ticks
             axis.ticks.y = element_blank()) + #remove y axis ticks
+      # scale_fill_viridis_d(direction = -1) +
       scale_fill_viridis_c(direction = 1) +
       theme(panel.background = element_rect(fill = "white"))
     
---
+++ heatmap/heatmap_plot.R
@@ -0,0 +1,65 @@
+# getwd()
+
+##########
+# Preprocessing: formatting table for heatmap
+# ---
+
+library(tidyverse)
+
+# data<-read.table("data/COVID-19_Singapore_Heatmap-table.tsv", header = TRUE, sep = "\t")
+data <- read.csv("data/COVID-19_Singapore_Heatmap-table.csv")
+
+data_long <- data %>% gather(key=date, value=status, X1.18.2020:X2.23.2020)
+
+data_long$date <- gsub('X', '0', data_long$date)
+data_long$date <- gsub('\\.', '\\/', data_long$date)
+
+write.csv(data_long, "data/COVID-19_Singapore_Heatmap_long_23-02-2020.csv")
+
+##########
+
+##########
+# Plotting
+
+library(ggplot2)
+library(viridis)
+library(plotly)
+
+data <- read.csv("data/COVID-19_Singapore_Heatmap_long_23-02-2020.csv")
+data$date <- factor(data$date, levels=unique(data$date))
+data$case <- factor(data$case, levels=unique(data$case))
+
+data$status_word=ifelse(data$status == 0,"Unexposed",
+                         ifelse(data$status == 1,"Exposed",
+                                ifelse(data$status == 2,"Symptomatic",
+                                       ifelse(data$status == 3,"Hospitalized","Discharged"))))
+
+write.csv(data, "data/COVID-19_Singapore_Heatmap_plot.csv")
+
+p1 <- ggplot(
+  data, 
+  # aes(x = date, y = case, fill = status_word,
+  aes(x = date, y = case, fill = status,
+      text = paste("Case: ", case,
+                   "<br>Date: ", date,
+                   "<br>Status: ", status_word,
+                   "<br>Cluster: ", cluster,
+                   "<br>Citizenship: ", citizenship))) +
+  geom_tile() +
+  xlab(label = "Date") +
+  ylab(label = "Cases") +
+  ggtitle("COVID-19 Progression Amongst Singapore Cases") +
+  labs(fill = "Status") + #tile fill legend label
+  theme(plot.title = element_text(hjust = 0.5)) + #centre main title
+  theme(axis.text.x = element_text(angle = 60, hjust = 0.6, size = 8),
+        axis.ticks.x = element_blank(), #remove x axis ticks
+        axis.ticks.y = element_blank()) + #remove y axis ticks
+  # scale_fill_viridis_d(direction = -1) +
+  scale_fill_viridis_c(direction = 1) +
+  theme(panel.background = element_rect(fill = "white"))
+
+ggplotly(p1,tooltip = 'text')
+
+##########
+
+ 
